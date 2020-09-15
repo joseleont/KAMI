b9a4fd523adcd46d1c22870b8b9c1a5f45ed2bbf
José León <jose.leont@pucp.pe>
2020-09-15 07:55:18 -0500

    SOLUCUOIN

--- src/com/company/ClaseB.java
+++ src/com/company/ClaseB.java
@@ -4,5 +4,8 @@ public class ClaseB {
 
     public void metodoB(){}
 
-
+/*
+ClaseB BES=new ClaseB();
+        BES.metodoB();
+ */
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        ClaseB BES=new ClaseB();
 
-        BES.metodoB();
+
 
 
     }
