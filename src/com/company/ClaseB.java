48652480bb63fd937a13d40457c0702fab2c11c7
José León <jose.leont@pucp.pe>
2020-09-15 07:45:44 -0500

    JUAN

---
+++ src/com/company/ClaseB.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class ClaseB {
+
+    public void metodoB(){}
+
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,17 @@
 package com.company;
 
+import sun.misc.Cleaner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        ClaseB BES=new ClaseB();
+
+        BES.metodoB();
+
+
     }
 
 }
